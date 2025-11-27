0466324a7b1fa932edf4f0bcd551a0fae054205c
PanchenkoEvgen <panchenkoevgen2004@gmail.com>
2025-11-27 02:14:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,22 +2,24 @@ import math
 import matplotlib.pyplot as plt
 import random
 from matplotlib.animation import FuncAnimation
+from matplotlib.colors import Normalize
+import numpy as np
 
-N = 20 #кількість "мурах"
+N = 50 #кількість "мурах"
 T = 100 #кількість переміщень
 
-x_min = 0
-x_max = 100
-y_min = 0
-y_max = 100
-V_min = -5 #для задання швидкості
-V_max = 5
+x_min = -4
+x_max = 4
+y_min = -4
+y_max = 4
+V_max = 0.1
+V_min = -V_max #для задання швидкості
 
 g = [15,39]
 
 w = 0.7
 wp = 2
-wg = 2
+wg = 3
 
 class Ant:
     def __init__(self,n, x, y, vx, vy, px, py, rp, rg):
@@ -34,17 +36,17 @@ class Ant:
         self.x = self.x + self.vx
         if self.x > x_max:
             self.vx = -self.vx
-            self.x = 100
+            self.x = x_max
         if self.x < x_min:
             self.vx = -self.vx
-            self.x = 0
+            self.x = x_min
         self.y = self.y + self.vy
         if self.y > y_max:
             self.vy = -self.vy
-            self.y = 100
+            self.y = x_max
         if self.y < y_min:
             self.vy = -self.vy
-            self.y = 0
+            self.y = x_min
         if func(self.x, self.y) < func(self.px, self.py):
             self.px = self.x
             self.py = self.y
@@ -63,8 +65,16 @@ class Ant:
 
 
 def func(x, y):
-    # return (x-50)**2 + (y-50)**2
-    return x+y
+    return (0.2 * (abs(x) + abs(y)) 
+            - 0.6 * math.cos(3*x + y) 
+            - 0.5 * math.sin(x*y) 
+            - 0.4 * abs(math.cos(2*x - y)))
+
+def func_np(x, y):
+    return (0.2 * (np.abs(x) + np.abs(y)) 
+            - 0.6 * np.cos(3*x + y) 
+            - 0.5 * np.sin(x*y) 
+            - 0.4 * np.abs(np.cos(2*x - y)))
 
 def generate_swarm(num):
     swarm = []
@@ -77,16 +87,20 @@ def generate_swarm(num):
         Vi1 = random.uniform(V_min, V_max)
         Vi2 = random.uniform(V_min, V_max)
         rp = random.uniform(0,1)
-        rg = random.uniform(0,1)
+        rg = 1-rp
         swarm.append(Ant(i+1, xi, yi, Vi1, Vi2, xi, yi, rp, rg))
     return swarm
 
 def move_phase(swarm):
     global g
     for a in swarm:
-        a.move()
+        # print(a.n)
+        # print([a.x, a.y], g)
+        # print(func(a.x, a.y), func(g[0],g[1]))
         if func(a.x, a.y) < func(g[0],g[1]):
             g = [a.x, a.y]
+        # print(g)
+        a.move()
 
 fig, ax = plt.subplots()
 
@@ -101,11 +115,26 @@ def update(frame):
     ax.set_title(f"Слайд {frame}")
     ax.scatter(x_max, y_max, color = "white")
     ax.scatter(x_min, y_min, color = "white")
+    x = np.linspace(x_min, x_max, 100)
+    y = np.linspace(y_min, y_max, 100)
+    X, Y = np.meshgrid(x, y)
+    t = func_np(X, Y)
+    ax.imshow(t, extent=[x_min, x_max, y_min, y_max], origin='lower',
+    aspect='equal')
     move_phase(swarm)
     for a in swarm:
-            a.stats()
+            # a.stats()
             ax.scatter(a.x, a.y, color = "orange")
 
 ani = FuncAnimation(fig, update, frames=T, interval=500)
 plt.show()
 print(g)
+print(func(g[0], g[1]))
+print(func_np(g[0], g[1]))
+x = np.linspace(x_min, x_max, 100)
+y = np.linspace(y_min, y_max, 100)
+X, Y = np.meshgrid(x, y)
+t = func_np(X, Y)
+plt.imshow(t, extent=[x_min, x_max, y_min, y_max], origin='lower', aspect='equal')
+plt.scatter(g[0],g[1], color = "pink")
+plt.show()
